10d5531542e528ca5d199038c7c41c5b213ac578
David Barbato <daviddev1306@gmail.com>
2022-08-21 17:23:11 -0300

    add test to user list

--- src/app.js
+++ src/app.js
@@ -6,4 +6,14 @@ app.get('/', (req, res) => {
     res.status(200).send();
 });
 
+app.get('/users', (req, res) => {
+    const users = [
+        {
+            name: 'Jose Joao',
+            email: 'jj@email.com',
+        },
+    ];
+    res.status(200).json(users);
+});
+
 module.exports = app;
