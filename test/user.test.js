9aedd2b5202c712ce0c5d43f1f5e9865c0186f46
David Barbato <92690205+dvbarbato@users.noreply.github.com>
2022-08-21 17:27:57 -0300

    add test to user list (#1)

---
+++ test/user.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+test('Deve listar todos os usuarios', async () => {
+    const res = await request(app).get('/users');
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveLength(1);
+    expect(res.body[0]).toHaveProperty('name', 'Jose Joao');
+});
